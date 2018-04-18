31f78c0e6f3dfe3a6abd2ef936f834f125de1cdc
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-04-18 19:14:37 +0200

    Simplified ios_system compilation

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,7 +87,7 @@ fi
 # compile ios_system where we want to find it:
 echo "Compiling ios_system:" 
 pushd $IOS_SYSTEM
-xcodebuild -project ios_system.xcodeproj -alltargets -sdk iphoneos -configuration Debug -quiet
+xcodebuild -project ios_system.xcodeproj -target ios_system -sdk iphoneos -configuration Debug -quiet
 popd
 
 # TODO: some combination of build variables might allow us to build these too. 
