885b0f7f8dc9b47d528299f4a97541979955e6ba
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-04-28 18:24:55 +0200

    commit for merge

--- include/llvm-c/Transforms/InstCombine.h
+++ include/llvm-c/Transforms/InstCombine.h
@@ -26,34 +26,6 @@ extern "C" {
  * @ingroup LLVMCTransforms
  *
  * @{
-/*===-- Scalar.h - Scalar Transformation Library C Interface ----*- C++ -*-===*\
-|*                                                                            *|
-|*                     The LLVM Compiler Infrastructure                       *|
-|*                                                                            *|
-|* This file is distributed under the University of Illinois Open Source      *|
-|* License. See LICENSE.TXT for details.                                      *|
-|*                                                                            *|
-|*===----------------------------------------------------------------------===*|
-|*                                                                            *|
-|* This header declares the C interface to libLLVMInstCombine.a, which        *|
-|* combines instructions to form fewer, simple IR instructions.               *|
-|*                                                                            *|
-\*===----------------------------------------------------------------------===*/
-
-#ifndef LLVM_C_TRANSFORMS_INSTCOMBINE_H
-#define LLVM_C_TRANSFORMS_INSTCOMBINE_H
-
-#include "llvm-c/Types.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/**
- * @defgroup LLVMCTransformsInstCombine Instruction Combining transformations
- * @ingroup LLVMCTransforms
- *
- * @{
  */
 
 /** See llvm::createInstructionCombiningPass function. */
@@ -68,5 +40,4 @@ void LLVMAddInstructionCombiningPass(LLVMPassManagerRef PM);
 #endif /* defined(__cplusplus) */
 
 #endif
-
 
